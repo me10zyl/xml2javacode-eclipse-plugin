3c4796704b1fbfb5c68d5eddf4175555269b1f02
zyl <zengyil@sunyuki.com>
2015-07-15 13:58:12 +0800

    eclipse luna runs ok

---
+++ xml2javacode.website/upload.bat
@@ -0,0 +1,3 @@
+cd E:\JavaEE\xml2javacode-plugin\xml2javacode.website\
+e:
+rsync --delete -rPv --exclude 'upload.bat' * root@zyl-me.xicp.net:/mnt/diskd7/linux/tomcat/apache-tomcat-7.0.54/webapps/xml2javacode/
